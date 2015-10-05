3fc022acea35a6311d89cc33d429693727cbf7c7
Dave Leach <dave@dmleach.com>
2015-10-05 14:53:46 -0400

    Adapted nav menu to build from WordPress categories

--- header.php
+++ header.php
@@ -11,31 +11,34 @@ Version    : 1.0
 Released   : 20130731
 -->
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css" media="all" />
-<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
-
-<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
-
-</head>
-<body>
-<div id="logo" class="container">
-	<h1><a href="#"><?php bloginfo('name') ?></a></h1>
-	<p>Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a></p>
-</div>
-<div id="menu" class="container">
-	<ul>
-		<li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-		<li><a href="#" accesskey="1" title="">Services</a></li>
-		<li><a href="#" accesskey="2" title="">Our Clients</a></li>
-		<li><a href="#" accesskey="3" title="">About Us</a></li>
-		<li><a href="#" accesskey="4" title="">Careers</a></li>
-		<li><a href="#" accesskey="5" title="">Contact Us</a></li>
-	</ul>
-</div>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+	<head>
+		<?php wp_head(); ?>
+		<title><?php bloginfo('name'); ?> <?php wp_title('-'); ?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+		<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css" media="all" />
+		<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" /><link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	</head>
+	<body>
+		<div id="logo" class="container">
+			<h1><a href="#"><?php bloginfo('name') ?></a></h1>
+			<p><?php bloginfo('description') ?></p>
+		</div>
+		<div id="menu" class="container">
+			<ul>
+				<li><a href="<?php get_option('home'); ?>" accesskey="1" title="">Home</a></li>
+				 <?php wp_list_categories(array (
+				 	"depth" => 1,
+					"exclude" => "1",
+				 	"hide_empty" => 0,
+					"orderby" => "ID",
+					"show_option_none" => "",
+					"title_li" => "",
+					"use_desc_for_title" => 0)
+				); ?>
+			</ul>
+		</div>
